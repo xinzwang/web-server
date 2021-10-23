5ed77059319870f42b6841799b470ca2e548a08a
WXZ <1790828743@qq.com>
2021-10-23 22:23:49 +0800

    提交学生信息

--- webroot/cgi-bin/query_submit.py
+++ webroot/cgi-bin/query_submit.py
@@ -1,11 +1,27 @@
 import os
 import sys
 import json
+import pymysql
 
 data = json.loads(sys.argv[1])
 
 uname = data['uname']
-usex = data['usex']
+uclass = data['uclass']
 unumber = data['unumber']
 
-rsp = None
+db = pymysql.connect(host="localhost", user="root",
+                     password="123456", database="db_student")
+cursor = db.cursor()
+
+sql = "INSERT INTO student VALUES(0,'"+unumber+"','"+uname+"','"+uclass+"')"
+
+rsp = {}
+try:
+    cursor.execute(sql)
+    db.commit()
+    rsp['msg'] = "success"
+except:
+    db.rollback()  # 错误时回滚
+    rsp['msg'] = "failed"
+
+print(json.dumps(rsp))
