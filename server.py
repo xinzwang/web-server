4e4f79cd3490b5f180d338d690d20cb7bc2bed6a
WXZ <1790828743@qq.com>
2021-11-25 21:35:09 +0800

    调整

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from application import WSGIServer
 ENABLE_HTTPS = False
 if __name__ == '__main__':
     # 创建服务器对象
-    wsgiServer = WSGIServer(enable_https=ENABLE_HTTPS)
+    wsgiServer = WSGIServer(enable_https=ENABLE_HTTPS, port=14001)
 
     wsgiServer.startServer()
     pass
